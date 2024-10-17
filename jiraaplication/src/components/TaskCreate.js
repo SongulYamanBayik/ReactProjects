c422ba6ed0f68426e2b21bb94b941f5429cd859d
Songül Yaman Bayık <snglymn@gmail.com>
2024-10-17 15:22:33 +0300

    .

--- jiraaplication/src/components/TaskCreate.js
+++ jiraaplication/src/components/TaskCreate.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function TaskCreate({ onCreate, task, taskformUpdate }) {
+function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
   const [title, setTitle] = useState(task ? task.title : '');
   const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : '');
 
@@ -12,7 +12,12 @@ function TaskCreate({ onCreate, task, taskformUpdate }) {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    onCreate(title, taskDesc);
+    if (taskformUpdate) {
+      onUpdate(task.id, title, taskDesc);
+    } else {
+      onCreate(title, taskDesc);
+    }
+
     setTitle('');
     setTaskDesc('');
   };
