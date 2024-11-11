7dc036a6ac5e25bc5bf3e2b7b0098a8c711308ab
Songül Yaman Bayık <snglymn@gmail.com>
2024-11-11 11:55:32 +0300

    .

--- courses/src/App.js
+++ courses/src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Courses from './Courses';
+import Loading from './Loading';
 
 function App() {
-  return <div className="App">dasdasda</div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:3000/courses');
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+
+    debugger;
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+  return (
+    <div className="App">
+      {loading ? <Loading /> : <Courses courses={courses} />}
+    </div>
+  );
 }
 
 export default App;
