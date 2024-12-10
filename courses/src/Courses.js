ac606b184b0509c274e60b7913f79a6d7b4a844d
Songül Yaman Bayık <snglymn@gmail.com>
2024-12-10 16:08:47 +0300

    .

--- courses/src/Courses.js
+++ courses/src/Courses.js
@@ -1,6 +1,11 @@
 import Course from './Course';
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 function Courses({ courses, removeCourse }) {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
   //   console.log(courses);
   return (
     <div className="courseMainDiv">
@@ -8,16 +13,19 @@ function Courses({ courses, removeCourse }) {
         <h2>Kurslarım</h2>
       </div>
       <div className="cardDiv">
-        {courses.map((course) => {
-          return (
-            <Course
-              key={course.id}
-              {...course}
-              removeOneCourse={removeCourse}
-            />
-          );
-          //   course={course}
-        })}
+        <button className="prevNextBtn">
+          <FaChevronLeft />
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price}TL</h4>
+          </div>
+          <p>{content}</p>
+        </div>
+        <button className="prevNextBtn">
+          <FaChevronRight />
+        </button>
       </div>
     </div>
   );
